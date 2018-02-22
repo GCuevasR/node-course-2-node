83565e8e7fc42b8f6b3fd1677f1655a064dfc810
Guilberto Cuevas Ripalda <guilberto.cuevas@gmail.com>
2018-02-22 00:57:23 -0300

    Added queries playground file

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,39 @@
+const {ObjectID} = require('mongodb');
+
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+
+// var id = '5a8e394b7b9abb5e32f11e31';
+//
+// if (!ObjectID.isValid(id)) {
+//   console.log('ID not valid');
+// }
+//
+// Todo.find({
+//   _id: id
+// }).then((todos) => {
+//   console.log('Todos', todos);
+// });
+//
+// Todo.findOne({
+//   _id: id
+// }).then((todo) => {
+//   console.log('Todo', todo);
+// });
+//
+// Todo.findById(id).then((todo) => {
+//   if (!todo) {
+//     return console.log('Id not found');
+//   }
+//   console.log('Todo By Id', todo);
+// }).catch((e) => console.log(e));
+
+User.findById('5a8b90ec7ec079cd464aa6c8').then((user) => {
+  if (!user) {
+    return console.log('Id not found');
+  }
+  console.log('User By Id', user);
+}, (e) => {
+  console.log(e);
+});
